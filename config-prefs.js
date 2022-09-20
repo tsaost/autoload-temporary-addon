e7c50ca18afd0368abab49c794314c055baa959b
Sheng-Te Tsao <tsaost@gmail.com>
2022-09-19 20:15:45 -0400

    Add files via upload
    This file is used to enable the AutoConfig aka userChrome.js feature of Firefox.  You need to copy it to [Your Firefox install directory]/defaults/pref
    
    For example, on Windows the location should be "c:\Program Files (x86)\Mozilla Firefox\defaults\pref" for the 32-bit version of Firefox.
    
    See https://www.userchrome.org/what-is-userchrome-js.html for a good explanation of AuctoConfig/userChrome.js

---
+++ config-prefs.js
@@ -0,0 +1,6 @@
+// config-prefs.js file for [Firefox program folder]\defaults\pref
+pref("general.config.obscure_value", 0);
+// the file named in the following line must be in [Firefox program folder]
+pref("general.config.filename", "userChrome.js");
+// disable the sandbox to run unsafe code
+pref("general.config.sandbox_enabled", false);
